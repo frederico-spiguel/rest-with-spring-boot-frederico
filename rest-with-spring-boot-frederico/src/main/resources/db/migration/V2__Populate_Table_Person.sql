1d45d4ed578834bb4060cf72e47c8a89ca97b992
frederico-spiguel <frederico.spiguel@gmail.com>
2025-03-19 21:52:39 -0300

    added content negotiation to support xml and yml

---
+++ rest-with-spring-boot-frederico/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `person` (`id`, `first_name`, `gender`, `last_name`, `address`) VALUES
+	(1, 'Frederiksen', 'Male', 'Pacheco', 'Copacabana - RJ'),
+	(3, 'Samuel', 'Male', 'Jackson', 'Los Angeles - LA'),
+	(5, 'Beetlejuice', 'Male', 'Silva', 'Los Angeles - Maranhão'),
+	(6, 'Ada', 'Female', 'Lovelace', 'New York - NY'),
+	(7, 'Nelsonete', 'Female', 'Mândela', 'Johannesburgo - SA'),
+	(8, 'Nelsonete', 'Female', 'Mândela', 'Johannesburgo - SA'),
+	(9, 'Nelsonete', 'Female', 'Mândela', 'Johannesburgo - SA'),
+	(10, 'Jack ', 'Male', 'Johnson', 'Australia - SA');
